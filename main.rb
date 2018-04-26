27c35068dd7be05750e064ce55228667cab3b5cf
Maciej Smusz <maciejsmusz@gmail.com>
2018-04-25 18:16:09 -0600

    first commit

---
+++ main.rb
@@ -0,0 +1,55 @@
+require 'sinatra'
+enable :sessions
+
+get '/' do
+  erb(:index)
+end
+
+get '/story/:title' do
+  @title = params[:title]
+  erb(:story)
+end
+
+post '/story/:title' do
+  @title = params[:title]
+  @post = params
+  puts params
+  @counter = 0
+  @text = ''
+  File.open("./public/uploads/#{@title}.txt", 'r') do |f|
+    f.each_line do |line|
+      line.split(' ').each do |word|
+        if word.start_with?('[') && word.end_with?(']')
+          # IT'S THIS LINE
+          @text << params[:param0]
+          @counter += 1
+        else
+          @text << word
+        end
+      end
+    end
+  end
+  session[:text] = @text
+  session[:title] = @title
+  redirect '/result'
+end
+
+get '/result' do
+  puts "Got to result"
+  @text = session[:text]
+  @title = session[:title]
+  erb('/result')
+end
+
+get '/upload' do
+  erb(:upload)
+end
+
+post '/upload' do
+  @filename = params[:file][:filename]
+  file = params[:file][:tempfile]
+  File.open("./public/uploads/#{@filename}", 'wb') do |f|
+    f.write(file.read)
+  end
+  redirect '/'
+end
